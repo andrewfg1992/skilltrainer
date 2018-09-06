f6538a4e5b549ec790905334274d97ef40b38a67
Andrew Flewellen-Gore <andrewfg1992@gmail.com>
2018-09-06 17:35:18 -0400

    Implement skeleton main() function

---
+++ src/skilltrainer.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+int main()
+{
+  
+  std::string choice("");
+  bool quit = false;
+  while(!quit)
+  {
+    std::cout << "Enter the number corresponding to what you want to train"
+              << std::endl
+              << "--------------------------------------------------------"
+              << std::endl
+              << "1. Math" << std::endl
+              //  New menu entries go here
+              << "   Enter 'q' to quit" << std::endl
+              << "--------------------------------------------------------"
+              << std::endl << std::endl;
+    
+    std::cin >> choice;
+    std::cin.clear();
+    std::cin.ignore();
+   
+    while(choice.length() != 1)
+    {
+      std::cout << "Do not enter more than one character, please. Try again"
+                << std::endl;
+      std::cin >> choice;
+      std::cin.clear();
+      std::cin.ignore();
+    }
+
+    char choice_char = choice.at(0);
+    switch(choice_char)
+    {
+      
+      case '1':
+        std::cout << "Math option selected" << std::endl;
+        // TODO call math option function
+        break;
+
+      case 'q':  // Ascii for 'q' -> quit
+        std::cout << "Quiting SkillTrainer. Thanks for playing!" << std::endl;
+        quit = true;
+        break;
+
+      default:
+        std::cout << "Invalid menu input: " << choice << ". Try again." << std::endl;
+        //std::string choice2("");
+        //std::cin >> choice2;
+        //std::cout << "next char: " << choice2 << std::endl;
+        //return 0;
+        break;
+    }
+  }
+  return 0;
+}
